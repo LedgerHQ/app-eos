4e198af690f6a02203b0bda919afc341466bdf68
Alexis Grojean <alexis.grojean@ledger.fr>
2025-10-23 15:53:49 +0200

    Bump version

--- src/ui.h
+++ src/ui.h
@@ -7,7 +7,7 @@
 #elif defined(TARGET_STAX) || defined(TARGET_FLEX)
 #define ICON_APP_EOS     C_app_eos_64px
 #define ICON_APP_HOME    ICON_APP_EOS
-#define ICON_APP_WARNING C_Warning_64px
+#define ICON_APP_WARNING LARGE_WARNING_ICON
 #elif defined(TARGET_APEX_P)
 #define ICON_APP_EOS     C_app_eos_48px
 #define ICON_APP_HOME    ICON_APP_EOS
